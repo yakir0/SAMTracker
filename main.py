972f9c17f1075aa8aa0bc5c130fcbd186f6d7313
yakir0 <yakird99@gmail.com>
2023-07-18 21:19:57 +0300

    add multi object tracking

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ import cv2
 import numpy as np
 
 from SAM import FSAM, FacebookSAM
-from utils import Bbox
+from utils import Bbox, draw_rect, draw_mask
 
-save = True
+save = False
 display = True
 debug = False
-save = display = False
+# save = display = False
 benchmark = not save and not display and not debug
 
 MAX_AGE = 20
 IOU_THRESH = 0.4
 
-video_file = 'dog'
+# video_file = 'dog'
 # video_file = 'surfer'
-# video_file = 'traffic'
+video_file = 'traffic'
 # video_file = 'traffic2'
 
 device = 'cpu'
-# sam = FSAM(small=False, device=device)
-sam = FacebookSAM(model="vit_h", device=device)
+sam = FSAM(small=True, device=device)
+# sam = FacebookSAM(model="vit_l", device=device)
 
 # Initialize the video capture
 video_capture = cv2.VideoCapture(f'videos/{video_file}.mp4')
@@ -40,32 +40,29 @@ ret, image = video_capture.read()
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 original_image = image.copy()
 
-# bbox = cv2.selectROI("Object Selection", original_image, fromCenter=False, showCrosshair=True)
-# bbox = Bbox(*bbox)
-bbox = Bbox(640, 380, 815, 550)  # dog
+bboxes = cv2.selectROIs("Object Selection", original_image, fromCenter=False, showCrosshair=True)
+bboxes = [Bbox(*bbox) for bbox in bboxes]
+# bbox = Bbox(640, 380, 815, 550)  # dog
 # bbox = Bbox(465, 282, 229, 200)  # surfer
 
 sam.set_image(image_rgb)
-ann = sam.box(bbox)
-prev_bbox = Bbox.from_mask(ann)
+prev_bboxes = []
+for bbox in bboxes:
+    ann = sam.box(bbox)
+    prev_bbox = Bbox.from_mask(ann)
+    prev_bboxes.append([prev_bbox, 0])
 
-if save or display:
-    if debug:
-        masked_img = np.where(ann[..., None], [0, 0, 255], original_image)
-        original_image = cv2.addWeighted(original_image, 0.8, masked_img, 0.2, 0, dtype=0)
-        cv2.rectangle(original_image, (prev_bbox.x, prev_bbox.y), (prev_bbox.x2, prev_bbox.y2), (255, 0, 0), 2)
-    cv2.rectangle(original_image, (bbox.x, bbox.y), (bbox.x2, bbox.y2), (0, 255, 0), 2)
+    if save or display:
+        if debug:
+            original_image = draw_mask(original_image, ann, (0, 0, 255))
+            original_image = draw_rect(original_image, prev_bbox, (255, 0, 0))
+        original_image = draw_rect(original_image, bbox, (0, 255, 0))
+
 if save:
     result.write(original_image)
 
-# if display:
-#     cv2.rectangle(original_image, (res_bbox.x, res_bbox.y), (res_bbox.x2, res_bbox.y2), (255, 0, 0), 2)
-#     cv2.imshow("res", original_image)
-#     cv2.waitKey(0)
-
 t1 = time.time()
 frame_count = 0
-dead_frames = 0
 while True:
     ret, image = video_capture.read()
     if not ret:
@@ -73,28 +70,26 @@ while True:
     if display or save:
         original_image = image.copy()  # Create a copy for visualization
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # detect
+    sit = time.time()
+    sam.set_image(image_rgb)
+    print(f'took: {time.time() - sit} sec')
 
-    if dead_frames <= MAX_AGE:
-        # detect
-        sit = time.time()
-        sam.set_image(image_rgb)
-        print(f'took: {time.time() - sit} sec')
-        ann = sam.box(prev_bbox)
-        ann_bbox = Bbox.from_mask(ann)
+    for i, (prev_bbox, dead_frames) in enumerate(prev_bboxes):
+        if dead_frames <= MAX_AGE:
+            ann = sam.box(prev_bbox)
+            ann_bbox = Bbox.from_mask(ann)
 
-        if ann_bbox and prev_bbox.iou(ann_bbox) > IOU_THRESH:
-            if display or save:
-                if debug:
-                    masked_img = np.where(ann[..., None], [0, 0, 255], original_image)
-                    original_image = cv2.addWeighted(original_image, 0.8, masked_img, 0.2, 0, dtype=0)
-                    cv2.rectangle(original_image, (prev_bbox.x, prev_bbox.y), (prev_bbox.x2, prev_bbox.y2), (255, 0, 0), 2)
-                cv2.rectangle(original_image, (ann_bbox.x, ann_bbox.y), (ann_bbox.x2, ann_bbox.y2), (0, 255, 0), 2)
-            prev_bbox = ann_bbox
-            dead_frames = 0
-        else:
-            dead_frames += 1
-    elif benchmark:
-        break
+            if ann_bbox and prev_bbox.iou(ann_bbox) > IOU_THRESH:
+                prev_bboxes[i][0] = ann_bbox
+                prev_bboxes[i][1] = 0
+                if display or save:
+                    if debug:
+                        original_image = draw_mask(original_image, ann, (0, 0, 255))
+                        original_image = draw_rect(original_image, prev_bbox, (255, 0, 0))
+                    original_image = draw_rect(original_image, ann_bbox, (0, 255, 0))
+            else:
+                prev_bboxes[i][1] += 1
 
     frame_count += 1
     if save:
@@ -106,10 +101,6 @@ while True:
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q'):
             break
-        if key & 0xFF == ord('p'):
-            tmp = sam.everything()
-            sam.prompt_process.plot(annotations=tmp,output='./output/tmp.jpg',)
-            cv2.waitKey(0)
 t2 = time.time()
 fps = frame_count / (t2 - t1)
 print(f'FPS: {fps}')
